fe7ae2e0699536f9461a3374949a7e552469d1bd
huangguojie880 <1583649520@qq.com>
2018-05-06 16:15:24 +0800

    Update simplecv2.py

--- simplecv2.py
+++ simplecv2.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from PIL import Image
 
+
 def resize_image_with_crop_or_pad(img, targe):
     """Crops and/or pads an image to a target width and height.
     Resizes an image to a target width and height by either centrally
@@ -56,7 +57,7 @@ def imcrop(img, box):
      top-left point and the coordinates of the bottom-right point.
      Horizontal x axis and vertical y axis.
     Args:
-      img: 3-D  shape `[height, width, channels]`.
+      img: 2D or 3D  
       box: tuple(x1,y1,x2,y2):(x1,y1)is top_left_point,(x2,y2)is right_lower_point
     Returns:
       A numpy array, img after crop.
@@ -68,14 +69,16 @@ def imcrop(img, box):
     x2 = box[2]
     y2 = box[3]
     if x1 >= x2 or y1 >= y2:
-        raise ('Error:The coordinates of the upper left point are not less than the coordinates of the lower right point')
-    if len(img_shape)== 3:
+        raise (
+        'Error:The coordinates of the upper left point are not less than the coordinates of the lower right point')
+    if len(img_shape) == 3:
         img_new = img[y1:y2, x1:x2, :]
-    elsif len(img_shape)== 2:
-	img_new = img[y1:y2, x1:x2]
+    elif len(img_shape) == 2:
+        img_new = img[y1:y2, x1:x2]
     else:
-	raise ('Error:img must 2D or 3D')
+        raise ('Error:img must 2D or 3D')
     return img_new
+
 
 def imshow(img):
     '''
@@ -84,10 +87,11 @@ def imshow(img):
     :param winname:Box name
     :return:None
     '''
-    cv2.imshow('img',np.uint8(img))
+    cv2.imshow('img', np.uint8(img))
     cv2.waitKey()
-    
-def imrotate(img,angle):
+
+
+def imrotate(img, angle):
     '''
     rotate a image
     :param img: Enter picture
@@ -99,23 +103,25 @@ def imrotate(img,angle):
     img_new = np.array(img_new)
     return img_new
 
-def imedge_canny(img, low = 50, high = 100):
-	'''
-	Use canny to get the edge of the image
-	:param img: gray or rgb
-	:param low:Canny's low threshold
-	:param high:Canny's high threshold
-	:return:edge img
-	'''
-	img_shape = img.shape
-	if len(img_shape) == 3:
-		gray = cv2.cvtColor(np.uint8(img), cv2.COLOR_BGR2GRAY)
-	else:
-		gray = img
-	canny = cv2.Canny(gray,low, high)
-	return canny
 
-def imextremum(img,type = 'max'):
+def imedge_canny(img, low=50, high=100):
+    '''
+    Use canny to get the edge of the image
+    :param img: gray or rgb
+    :param low:Canny's low threshold
+    :param high:Canny's high threshold
+    :return:edge img
+    '''
+    img_shape = img.shape
+    if len(img_shape) == 3:
+        gray = cv2.cvtColor(np.uint8(img), cv2.COLOR_BGR2GRAY)
+    else:
+        gray = img
+    canny = cv2.Canny(gray, low, high)
+    return canny
+
+
+def imextremum(img, type='max'):
     '''
     Find the maximum or minimum value of the picture
     :param img: -
