0a99238ca1353123509adf37a68d4256dcc4bf2b
huangguojie880 <1583649520@qq.com>
2018-05-23 10:14:27 +0800

    Update simplecv2.py

--- simplecv2.py
+++ simplecv2.py
@@ -4,52 +4,52 @@ from PIL import Image
 
 
 def resize_image_with_crop_or_pad(img, targe):
-	"""Crops and/or pads an image to a target width and height.
-	Resizes an image to a target width and height by either centrally
-	cropping the image or padding it evenly with zeros.
-	If `width` or `height` is greater than the specified `target_width` or
-	`target_height` respectively, this op centrally crops along that dimension.
-	If `width` or `height` is smaller than the specified `target_width` or
-	`target_height` respectively, this op centrally pads with 0 along that
-	dimension.
-	Args:
-	  img: 3-D  shape `[height, width, channels]`.
-	  targe: tuple `(targe_width,targe_height)`.
-	Returns:
-	  A numpy array, img after resize.
-	  a 3-D uint8 shape.
-	`[new_height, new_width, channels]`.
-	"""
-	targe_w = targe[0]
-	targe_h = targe[1]
-	h, w, deep_dim = img.shape[0], img.shape[1], img.shape[2]
-	w_half = int(np.floor(w / 2))
-	h_half = int(np.floor(h / 2))
-	img_new = np.uint8(np.zeros(shape=(targe_h, targe_w, deep_dim)))
-	targe_w_half = int(np.floor(targe_w / 2))
-	targe_h_half = int(np.floor(targe_h / 2))
-	if h > targe_h:
-		targe_h_s = 0
-		targe_h_e = targe_h + 1
-		h_s = h_half - targe_h_half
-		h_e = h_half - targe_h_half + targe_h
-	else:
-		targe_h_s = targe_h_half - h_half
-		targe_h_e = targe_h_half - h_half + h
-		h_s = 0
-		h_e = h + 1
-	if w > targe_w:
-		targe_w_s = 0
-		targe_w_e = targe_w + 1
-		w_s = w_half - targe_w_half
-		w_e = w_half - targe_w_half + targe_w
-	else:
-		targe_w_s = targe_w_half - w_half
-		targe_w_e = targe_w_half - w_half + w
-		w_s = 0
-		w_e = w + 1
-	img_new[targe_h_s:targe_h_e, targe_w_s:targe_w_e, :] = img[h_s:h_e, w_s:w_e, :]
-	return img_new
+    """Crops and/or pads an image to a target width and height.
+    Resizes an image to a target width and height by either centrally
+    cropping the image or padding it evenly with zeros.
+    If `width` or `height` is greater than the specified `target_width` or
+    `target_height` respectively, this op centrally crops along that dimension.
+    If `width` or `height` is smaller than the specified `target_width` or
+    `target_height` respectively, this op centrally pads with 0 along that
+    dimension.
+    Args:
+      img: 3-D  shape `[height, width, channels]`.
+      targe: tuple `(targe_width,targe_height)`.
+    Returns:
+      A numpy array, img after resize.
+      a 3-D uint8 shape.
+    [new_height, new_width, channels]`.
+    """
+    targe_w = targe[0]
+    targe_h = targe[1]
+    h, w, deep_dim = img.shape[0], img.shape[1], img.shape[2]
+    w_half = int(np.floor(w / 2))
+    h_half = int(np.floor(h / 2))
+    img_new = np.zeros(shape=(targe_h, targe_w, deep_dim))
+    targe_w_half = int(np.floor(targe_w / 2))
+    targe_h_half = int(np.floor(targe_h / 2))
+    if h > targe_h:
+        targe_h_s = 0
+        targe_h_e = targe_h + 1
+        h_s = h_half - targe_h_half
+        h_e = h_half - targe_h_half + targe_h
+    else:
+        targe_h_s = targe_h_half - h_half
+        targe_h_e = targe_h_half - h_half + h
+        h_s = 0
+        h_e = h + 1
+    if w > targe_w:
+        targe_w_s = 0
+        targe_w_e = targe_w + 1
+        w_s = w_half - targe_w_half
+        w_e = w_half - targe_w_half + targe_w
+    else:
+        targe_w_s = targe_w_half - w_half
+        targe_w_e = targe_w_half - w_half + w
+        w_s = 0
+        w_e = w + 1
+    img_new[targe_h_s:targe_h_e, targe_w_s:targe_w_e, :] = (img[h_s:h_e, w_s:w_e, :])
+    return img_new
 
 
 def imcrop(img, box):
@@ -85,14 +85,14 @@ def imcrop(img, box):
     return img_new
 
 def imshow(img):
-	'''
-	Simple image display
-	:param img: Enter picture
-	:param winname:Box name
-	:return:None
-	'''
-	cv2.imshow('img', np.uint8(img))
-	cv2.waitKey()
+    '''
+    Simple image display
+    :param img: Enter picture
+    :param winname:Box name
+    :return:None
+    '''
+    cv2.imshow('img', np.uint8(img))
+    cv2.waitKey()
 
 
 def imrotate(img, angle):
